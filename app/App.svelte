daa9b7d6ea0998d52f93b1165bd55730858eb0f0
Denzell Ford <fdenzell@gmail.com>
2020-04-09 18:51:02 -0500

    set svelte native

---
+++ app/App.svelte
@@ -0,0 +1,27 @@
+<page>
+    <actionBar title="Ratio Calcendar" />
+    <gridLayout>
+        <Calendar />
+        <label class="info" horizontalAlignment="center" verticalAlignment="center" textWrap="true">
+            <formattedString>
+                <span class="fas" text="&#xf135;" />
+                <span text=" {message}" />
+            </formattedString>
+        </label>
+    </gridLayout>
+</page>
+
+<script>
+    import Calendar from './Calendar.svelte';
+
+    let message = "Blank Svelte Native App"
+</script>
+
+<style>
+    .info .fas {
+        color: #3A53FF;
+    }
+    .info {
+        font-size: 20;
+    }
+</style>
